247c8f09ea9bb270a4ba1ede68ad1c4388bd4e0a
Balazs Szendro <peppincsoda@gmail.com>
2017-10-28 16:24:43 +0200

    sfv2: Error messages for serial port handling

--- filterapp/SerialOutput.cpp
+++ filterapp/SerialOutput.cpp
@@ -23,29 +23,41 @@ namespace sfv2 {
 
     bool SerialOutput::open(const FilterSettings& settings)
     {
-        auto r = true;
-
-        sp_ = std::make_unique<QSerialPort>();
         const auto& port_name = settings.serialPortName();
-        sp_->setPortName(port_name);
 
-        r = r && sp_->setBaudRate(settings.serialBaudRate());
-        r = r && sp_->setDataBits(static_cast<QSerialPort::DataBits>(
-            settings.serialDataBits()));
-        r = r && sp_->setParity(enumValueFromString<QSerialPort::Parity>(settings.serialParity()));
-        r = r && sp_->setStopBits(static_cast<QSerialPort::StopBits>(
-            settings.serialStopBits()));
-        r = r && sp_->setFlowControl(enumValueFromString<QSerialPort::FlowControl>(settings.serialFlowControl()));
+        sp_ = std::make_unique<QSerialPort>(port_name);
 
-        r = r && sp_->open(QSerialPort::WriteOnly);
-        if (!r) {
-            qWarning() << "Failed to open serial port: " << port_name;
+        if (!sp_->setBaudRate(settings.serialBaudRate())) {
+            qWarning() << "Invalid baud rate setting for serial port";
+            return false;
         }
+        if (!sp_->setDataBits(static_cast<QSerialPort::DataBits>(settings.serialDataBits()))) {
+            qWarning() << "Invalid data bits setting for serial port";
+            return false;
+        }
+        if (!sp_->setParity(enumValueFromString<QSerialPort::Parity>(settings.serialParity()))) {
+            qWarning() << "Invalid parity setting for serial port";
+            return false;
+        }
+        if (!sp_->setStopBits(static_cast<QSerialPort::StopBits>(settings.serialStopBits()))) {
+            qWarning() << "Invalid stop bits setting for serial port";
+            return false;
+        }
+        if (!sp_->setFlowControl(enumValueFromString<QSerialPort::FlowControl>(settings.serialFlowControl()))) {
+            qWarning() << "Invalid flow control setting for serial port";
+            return false;
+        }
+        if (!sp_->open(QSerialPort::WriteOnly)) {
+            qWarning() << "Failed to open serial port: " << port_name;
+            return false;
+        }
+
+        qInfo() << "Opened serial port:" << port_name;
 
         slave_address_ = settings.modbusSlaveAddress();
         data_address_ = settings.modbusDataAddress();
 
-        return r;
+        return true;
     }
 
     bool SerialOutput::write(const FilterOutputData& data)
